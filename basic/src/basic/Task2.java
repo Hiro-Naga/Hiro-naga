724dc16e9b30958bfac96336b9514e8a127d2f13
Hiro-Naga <nagasawa944@gmail.com>
2020-03-20 22:40:29 +0900

    課題提出 - Javaの基礎

---
+++ basic/src/basic/Task2.java
@@ -0,0 +1,11 @@
+package basic;
+
+public class Task2 {
+	public static void main(String args[]) {
+		//コメント　スラッシュが1重
+		
+		System.out.println("hogehoge"); //セミコロン抜け
+		System.out.println("hogehoge2"); //main文中にない
+		
+	}
+}
