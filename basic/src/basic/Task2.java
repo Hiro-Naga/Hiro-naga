085a1843a93155f184765a5044ee15b76ad5e676
Hiro-Naga <nagasawa944@gmail.com>
2020-03-27 17:51:25 +0900

    再提出 - Javaの基礎

--- basic/src/basic/Task2.java
+++ basic/src/basic/Task2.java
@@ -2,7 +2,8 @@ package basic;
 
 public class Task2 {
 	public static void main(String args[]) {
-		//コメント　スラッシュが1重
+		/*コメント /と*で囲まれていない
+		コメント*/
 
 		System.out.println("hogehoge"); 	//セミコロン抜け
 		System.out.println("hogehoge2"); 	//main文中にない
