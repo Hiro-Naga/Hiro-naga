cf836b544745f2e611272d5672cd8dc63d8da152
Hiro-Naga <nagasawa944@gmail.com>
2020-04-01 20:28:18 +0900

    再提出 - いろいろな配列

--- vrietyArray/src/vrietyArray/Task3.java
+++ vrietyArray/src/vrietyArray/Task3.java
@@ -5,16 +5,15 @@ import java.util.HashMap;
 public class Task3 {
 	public static void main(String args[]) {
 		HashMap<String, Integer> fruits = new HashMap<>();
-		fruits.put("りんご",130);
-		fruits.put("みかん",120);
-		fruits.put("バナナ",98);
-		fruits.put("メロン",6000);
-
+		fruits.put("りんご", 130);
+		fruits.put("みかん", 120);
+		fruits.put("バナナ", 98);
+		fruits.put("メロン", 6000);
 
 		for(HashMap.Entry<String, Integer> fruit: fruits.entrySet()) {
 			System.out.println(fruit.getKey() + "：" + fruit.getValue() + "円");
 		}
-		
+
 	}
 
 }
--- vrietyArray/src/vrietyArray/Task4.java
+++ vrietyArray/src/vrietyArray/Task4.java
@@ -5,12 +5,12 @@ import java.util.HashMap;
 
 public class Task4 {
 	public static void main(String args[]) {
-		
+
 		//変数設定
 		ArrayList<Integer> odds = new ArrayList<>();
 		ArrayList<Integer> evens = new ArrayList<>();
 		HashMap<String, ArrayList<Integer>> map = new HashMap<>();
-		
+
 		//奇数偶数の振り分け
 		int[] num = {21, 3, 32, 6, 99, 72, 78, 51, 1, 26, 87, 11, 48, 60, 29, 5, 46, 74, 14, 193, 215, 83};
 		for(int i = 0; i < num.length; i++) {
@@ -20,11 +20,11 @@ public class Task4 {
 				odds.add(num[i]);
 			}
 		}
-		
+
 		//HashMap生成・表示
-		map.put("奇数", odds);
-		map.put("偶数",evens);
-		
+		map.put("奇数: ", odds);
+		map.put("偶数: ", evens);
+
 		System.out.println(map);
 	}
 
