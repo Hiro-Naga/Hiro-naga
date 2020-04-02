715f4380ad6906c8ac8fc4da1d0411eaf01d40f6
Hiro-Naga <nagasawa944@gmail.com>
2020-04-02 21:13:51 +0900

    再々提出 - 配列

--- array/src/array/Task1.java
+++ array/src/array/Task1.java
@@ -3,10 +3,10 @@ package array;
 public class Task1 {
 	public static void main(String args[]) {
 		int[] array = {4, 9, 19, 5, 8, 21, 42, 64, 73, 18, 2};
-		
+
 		//配列表示しつつ合計を計算
 		int sum = 0;
-		
+
 		System.out.print("配列：[");
 		for(int i = 0; i < array.length; i++ ) {
 			if(i == array.length - 1) {
@@ -14,14 +14,13 @@ public class Task1 {
 			}else {
 				System.out.print(array[i] + ",");
 			}
-			//sum = sum + array[i];
 		}
-		
-		//for文中のコメント部分を拡張for文に
+
+		//合計計算のみ拡張for文に
 		for(int ele: array) {
 			sum = sum + ele;
 		}
-		
+
 		System.out.println("合計：" + sum);
 	}
 
